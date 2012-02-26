9df142887435c89c9d365c0311e8bf86876a8dfb
Larry Reid <lcreid@jadesystems.ca>
2012-02-26 09:00:35 -0800

    Added helper for ListStats

--- app/helpers/list_helper.rb
+++ app/helpers/list_helper.rb
@@ -1,2 +1,34 @@
 module ListHelper
+class ListStats
+	def setup
+		StudentResponse.all.destroy
+	end
+		
+	def total_words
+		ListItem.all.count
+	end
+	
+	def words_correct
+		# TODO: Should the following be word_id? 
+		# Not yet, because I don't always bother to set word_id.
+		# In the future, quite possibly
+		StudentResponse.select(:word).where(:correct => true).count(:distinct => true)
+	end
+	
+	def words_tried
+		# TODO: Should the following be word_id? 
+		# Not yet, because I don't always bother to set word_id.
+		# In the future, quite possibly
+		StudentResponse.select(:word).count(:distinct => true)
+	end
+		
+	def words_untried
+		total_words - words_tried
+	end
+	
+	def words_left
+		total_words - words_correct
+	end
+	
+end
 end
--- test/unit/helpers/list_helper_test.rb
+++ test/unit/helpers/list_helper_test.rb
@@ -1,4 +1,53 @@
 require 'test_helper'
 
 class ListHelperTest < ActionView::TestCase
+	test "results" do
+		ls = ListStats.new
+		assert_equal 20, ls.total_words, "Total words"
+		assert_equal 0, ls.words_correct, "Words correct"
+		assert_equal 20, ls.words_untried, "Words untried"
+		assert_equal 20, ls.words_left, "Words left"
+		
+		# Response is correct
+		r = StudentResponse.new
+		r.word = list_items(:each).word
+		r.student_response = list_items(:each).word
+		r.save # TODO: Should the model save itself? Should the stats depend on the database?
+		assert_equal 20, ls.total_words, "Total words"
+		assert_equal 1, ls.words_correct, "Words correct"
+		assert_equal 19, ls.words_untried, "Words untried"
+		assert_equal 19, ls.words_left, "Words left"
+		
+		# Response is wrong for new word
+		r = StudentResponse.new
+		r.word = list_items(:speech).word
+		r.student_response = "speach"
+		r.save # TODO: Should the model save itself? Should the stats depend on the database?
+		assert_equal 20, ls.total_words, "Total words"
+		assert_equal 1, ls.words_correct, "Words correct"
+		assert_equal 18, ls.words_untried, "Words untried"
+		assert_equal 19, ls.words_left, "Words left"
+		
+		# Response is correct for previously wrong word
+		r = StudentResponse.new
+		r.word = list_items(:speech).word
+		r.student_response = "speech"
+		r.save # TODO: Should the model save itself? Should the stats depend on the database?
+		assert_equal 20, ls.total_words, "Total words"
+		assert_equal 2, ls.words_correct, "Words correct"
+		assert_equal 18, ls.words_untried, "Words untried"
+		assert_equal 18, ls.words_left, "Words left"
+		
+		# Previously spelled correctly is correct again
+		r = StudentResponse.new
+		r.word = list_items(:each).word
+		r.student_response = list_items(:each).word
+		r.save # TODO: Should the model save itself? Should the stats depend on the database?
+		assert_equal 20, ls.total_words, "Total words"
+		assert_equal 2, ls.words_correct, "Words correct"
+		assert_equal 18, ls.words_untried, "Words untried"
+		assert_equal 18, ls.words_left, "Words left"
+		
+	end
+	
 end
