99c9faec2aa07456b14dbd51c20bd50d01de09b5
Larry Reid <lcreid@jadesystems.ca>
2012-04-04 21:16:43 -0700

    Change deploy.rb to include Passenger stuff.

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ role :db,  "passenger"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
- If you are using Passenger mod_rails uncomment this:
+# If you are using Passenger mod_rails uncomment this:
  namespace :deploy do
    task :start do ; end
    task :stop do ; end
