7eef427e73801330dc98b5c36a9b3d094d4d90d6
Larry Reid <lcreid@jadesystems.ca>
2012-04-04 21:14:32 -0700

    Change deploy.rb to include Passenger stuff.

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,11 +15,11 @@ role :db,  "passenger"
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ If you are using Passenger mod_rails uncomment this:
+ namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+ end
