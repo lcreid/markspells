8768dfc99ce3b12f5707563c57a435f9cf3cd15b
Larry Reid <lcreid@jadesystems.ca>
2012-04-27 21:46:02 -0700

    Set focus after add word.

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -3,8 +3,9 @@ function remove_fields(link) {
   $(link).closest(".fields").hide();
 }
 
-function add_fields(link, association, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
+function add_fields(link, association, content, new_id) {
+  new_id = typeof new_id !== 'undefined' ? new_id : new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g");
   $(link).parent().before(content.replace(regexp, new_id));
+  $("div.fields:last input:first").focus();
 }
