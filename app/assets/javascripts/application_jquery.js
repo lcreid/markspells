ca14db28a5a607fb2a31e08a8cf14bb54bf99dab
Larry Reid <lcreid@jadesystems.ca>
2012-04-28 07:00:42 -0700

    Set focus after add word. More robust.

--- app/assets/javascripts/application_jquery.js
+++ app/assets/javascripts/application_jquery.js
@@ -7,5 +7,8 @@ function add_fields(link, association, content, new_id) {
   new_id = typeof new_id !== 'undefined' ? new_id : new Date().getTime();
   var regexp = new RegExp("new_" + association, "g");
   $(link).parent().before(content.replace(regexp, new_id));
-  $("div.fields:last input:first").focus();
+  // Below added by LCR 2012-04-28
+  // Set focus on the first field of the added row.
+//  $("div.fields:last input:first").focus();
+  $(link).parent().prev().find(":input:first").focus();
 }
