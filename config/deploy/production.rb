436605c34154c73c92094ecd7db68076efb430a3
Larry Reid <lcreid@jadesystems.ca>
2012-05-05 10:04:30 -0700

    Change server because I rebuilt the AWS one.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,8 @@
 
 set :user, "ubuntu"
 
-role :web, "ec2-184-73-29-53.compute-1.amazonaws.com"                   # Your HTTP server, Apache/etc
-role :app, "ec2-184-73-29-53.compute-1.amazonaws.com"                   # This may be the same as your `Web` server
-role :db,  "ec2-184-73-29-53.compute-1.amazonaws.com", :primary => true # This is where Rails migrations will run
-role :db,  "ec2-184-73-29-53.compute-1.amazonaws.com"
+role :web, "ec2-204-236-210-66.compute-1.amazonaws.com"                   # Your HTTP server, Apache/etc
+role :app, "ec2-204-236-210-66.compute-1.amazonaws.com"                   # This may be the same as your `Web` server
+role :db,  "ec2-204-236-210-66.compute-1.amazonaws.com", :primary => true # This is where Rails migrations will run
+role :db,  "ec2-204-236-210-66.compute-1.amazonaws.com"
 
